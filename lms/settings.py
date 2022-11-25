bc485188cadbeb9e1fe22f70e2ce6f142c3b8131
OrhieAsoro <orhieasoro@gmail.com>
2022-11-25 06:55:38 -0700

    cloudinary storage

--- lms/settings.py
+++ lms/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
-import cloudinary
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 import dj_database_url
